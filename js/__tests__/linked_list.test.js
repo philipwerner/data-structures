505f2fb5efcd1b3f55d8ca73bee4f22432926508
Philip Werner <philip.r.werner@gmail.com>
2018-01-05 13:00:49 -0800

    l

---
+++ js/__tests__/linked_list.test.js
@@ -0,0 +1,16 @@
+let linkedList = require(../linked_list)
+
+test('Test there is no data on initialization', () => {
+    let l = new linkedList.LinkedList();
+    expect(l.head).toBeNull();
+});
+
+test('Test push method works', () => {
+    let l = new linkedList.LinkedList();
+    l.push(99)
+    l.push(88)
+    expect(l.head.val).toBe(88)
+    expect(l.head.next.val)toBe(99)
+});
+
+test
