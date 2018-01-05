6c817e30313447415902f08f7033172971f2374a
Philip Werner <philip.r.werner@gmail.com>
2018-01-05 15:43:34 -0800

    completed linked list tests, i think.

--- js/__tests__/linked_list.test.js
+++ js/__tests__/linked_list.test.js
@@ -29,3 +29,41 @@ test('Test pop method on empty linked list', () => {
     let l = new linkedList.LinkedList();
     expect(l.pop()).toBeNull()
 });
+
+test('Test pop method works', () => {
+    let l = new linkedList.LinkedList([99, 98]);
+    expect(l.pop()).toBe(99)
+    expect(l.head.val).toBe(98)
+});
+
+test('Test size method', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    expect(l.size()).toBe(4)
+})
+
+test('Test size after pop', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    l.pop()
+    expect(l.size()).toBe(3)
+})
+
+test('Test search method', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    expect(l.search(97).val).toBe(97)
+})
+
+test('Test search method when node not present', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    expect(l.search(79).val).toBeNull()
+})
+
+test('Test remove method', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    l.remove(99)
+    expect(l.size()).toBe(3)
+})
+
+test('Test display method', () => {
+    let l = new linkedList.LinkedList([99, 98, 97, 96]);
+    expect(l.display()).toBe('(96, 97, 98, 99)')
+})
