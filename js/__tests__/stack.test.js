e0430e55f5288bb58f6ec185db6bf026f02e04fb
Philip Werner <philip.r.werner@gmail.com>
2018-01-05 22:44:38 -0800

    working on more tests.

--- js/__tests__/stack.test.js
+++ js/__tests__/stack.test.js
@@ -2,7 +2,7 @@ let stack = require(../stack)
 
 test('Test there is no data on initialization', () => {
     let s = new stack.Stack();
-    expect(l.head).toBeNull();
+    expect(s.head).toBeNull();
 });
 
 test('Test push method works', () => {
@@ -12,3 +12,28 @@ test('Test push method works', () => {
     expect(s.head.val).toBe(50)
     expect(s.head.next.val)toBe(25)
 });
+
+test('Test pop method works', () => {
+    let s = new stack.Stack();
+    s.push(25)
+    s.push(50)
+    s.push(75)
+    expect(s.pop()).toBe(25)
+})
+
+test('Test multiple pops', () => {
+    let s = new stack.Stack();
+    s.push(25)
+    s.push(50)
+    s.push(75)
+    expect(s.pop()).toBe(25)
+    expect(s.pop()).toBe(50)
+})
+
+test('Test peek method works', () => {
+    let s = new stack.Stack();
+    s.push(25)
+    s.push(50)
+    s.push(75)
+    expect(s.peek()).toBe(75)
+})
